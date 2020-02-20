c4a88c61a785942319521aaf01b4d592acaf10a6
nikkilinx <nicole.l.lincoln@gmail.com>
2020-02-19 22:11:41 -0700

    basic structure created

---
+++ resume_sandbox/__init__.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = '3308',
+        DATABASE = os.path.join(app.instance_path, 'resume_sandbox.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def home():
+        return 'Hello, Team! Welcome to our new webpage'
+
+    return app
